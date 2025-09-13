2ffaed9b321dcf6e2bc79a233ea18bb42edd0877
YS <ys@workPC>
2025-09-14 02:33:18 +0800

    rewrite interface so that it extends from base profile. Extend Res locals to Reslocal

--- matcha-backend/src/model/profile.ts
+++ matcha-backend/src/model/profile.ts
@@ -1,28 +1,49 @@
-export interface ProfileJson {
+interface Profile {
   firstName: string;
   lastName: string;
   email: string;
   username: string;
   pw: string;
+}
+
+export interface ProfileRegJson extends Profile {
   pw2: string;
+  birthDate: string;
   biography?: string;
   gender?: string;
   sexualPreference?: string;
-  age?: number;
 };
 
-export interface ProfileDb {
-  firstName: string;
-  lastName: string;
-  email: string;
-  username: string;
-  pw: string;
-  pw2: string;
+export interface ProfileDb extends Profile{
+  id:string;
+  birthDate: Date;
   biography?: string;
   gender?: string;
   sexualPreference?: string;
-  age?: number;
   fameRating?: number;
+  photo0?: string;
+  photo1?: string;
+  photo2?: string;
+  photo3?: string;
+  photo4?: string;
+  activated: boolean;
   createdAt: string;
   updatedAt: string;
 };
+
+export interface ProfileUpdateJson {
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+  sexualPreference: string;
+  biography: string;
+  birthdate: Date;
+}
+
+export interface Reslocal{
+  authenticated:boolean;
+  username:string;
+  id:string;
+  activated:boolean;
+}
