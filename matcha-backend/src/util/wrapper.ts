bcf8218a877965ee0b3851179f87ff6d4d9b37ab
YS <ys@workPC>
2025-09-25 16:46:43 +0800

    new wrapper for simplifying error handling

--- matcha-backend/src/util/wrapper.ts
+++ matcha-backend/src/util/wrapper.ts
@@ -12,3 +12,25 @@ export const serverErrorWrapper = async <T>(fn: () => T, errorMsg: string, log: 
     });
   }
 };
+
+export const catchErrorWrapper = async <T>(promise : Promise<T>): Promise<[undefined, T] | [Error]> => {
+  try {
+    const result = await promise;
+    return [undefined, result] as [undefined, T];
+  } catch (error) {
+    return [error as Error];
+  }
+};
+
+export const catchTypedErrorWrapper = async <T, E extends new (message?:string) => Error>(promise: Promise<T>, ErrorTypetoCatch?: E[]): Promise<[undefined, T] | [InstanceType<E>]> => {
+  try {
+    const result = await promise;
+    return [undefined, result] as [undefined, T];
+  } catch (error) {
+    if (!ErrorTypetoCatch || ErrorTypetoCatch.length === 0) 
+      return [error as InstanceType<E>];
+    if (ErrorTypetoCatch.some((ErrorType) => error instanceof ErrorType))
+      return [error as InstanceType<E>];
+    throw error;
+  }
+};
